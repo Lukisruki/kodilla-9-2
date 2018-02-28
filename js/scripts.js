0c4c7cbc9e1d9e3f8466780c515bfe7c7d8d235b
lukasz kryst <lukaszkryst@gmail.com>
2018-02-28 12:16:26 +0100

    Edit scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ var nameMale = ['Jan', 'Tomek', 'Kuba'],
     nameFemale = ['Kasia', 'Aga', 'Ewa'];
 var allNames = nameMale.concat(nameFemale);
 
-var newName = 'Marian';
-allNames.splice(0, 0, newName);
+var newName = 'Marian' {
+    if allNames.indexOf(newName) === -1 {
+        allNames.push(newName);
+    };
+};
+
 // I'm totally lost :(...
 console.log(allNames);
